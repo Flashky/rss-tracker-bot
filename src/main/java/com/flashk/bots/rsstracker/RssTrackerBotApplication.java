5918a0af46a0dd35b598bcd79037dd908a159235
Flashk <mevengar@gmail.com>
2021-10-24 12:34:30 +0200

    Create project

---
+++ src/main/java/com/flashk/bots/rsstracker/RssTrackerBotApplication.java
@@ -0,0 +1,13 @@
+package com.flashk.bots.rsstracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RssTrackerBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RssTrackerBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flashk/bots/rsstracker/RssTrackerBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flashk.bots.rsstracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RssTrackerBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
