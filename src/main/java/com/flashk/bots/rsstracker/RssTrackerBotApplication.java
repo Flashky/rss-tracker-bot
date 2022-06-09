a3e82fa55371700221dc577fe6467dc7a9a33144
Flashk <mevengar@gmail.com>
2022-06-09 15:50:48 +0200

    Enable MongoDB auditing

--- src/main/java/com/flashk/bots/rsstracker/RssTrackerBotApplication.java
+++ src/main/java/com/flashk/bots/rsstracker/RssTrackerBotApplication.java
@@ -7,8 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
+@EnableMongoAuditing // Support for @CreatedDate and @LastModifiedDate on Documents
 public class RssTrackerBotApplication {
 
 	public static void main(String[] args) {
